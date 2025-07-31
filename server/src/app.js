3eca6648858aaa403e88307ebab6e06abb11c707
SamiMansuri <mohammadsamimansuri20@gnu.ac.in>
2025-07-31 21:07:58 +0530

    added cors

--- server/src/app.js
+++ server/src/app.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import indexRouter from "./routes/index.routes.js";
 import {
   errorHandler,
@@ -12,6 +13,7 @@ const app = express();
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors())
 
 app.use("/api/v1", indexRouter);
 
